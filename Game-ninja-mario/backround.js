622b6338cf5effbfd514f86fcbf15d472ba7888c
francisco cano <frankiecano24@gmail.com>
2022-02-23 13:56:07 -0500

    Solved lab

--- Game-ninja-mario/backround.js
+++ Game-ninja-mario/backround.js
@@ -2,19 +2,14 @@ const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
 let image = new Image();
-image.src ='https://cdna.artstation.com/p/assets/images/images/013/740/724/large/andrew-melfi-no-name-ninja-background.jpg?1540926668'
+image.src ="https://cdna.artstation.com/p/assets/images/images/013/740/724/large/andrew-melfi-no-name-ninja-background.jpg?1540926668"
 
 
 let i = 0;
 let j = canvas.width;
 
 function draw() {
-context.clearRect(-1, -2, canvas.width, canvas.height);
-
-const drawImage =() =>{
-
-  }; 
-  i--; 
+context.clearRect(0, 0, canvas.width, canvas.height);
 
   if (i <= -canvas.width) {
     i = canvas.width;
@@ -26,12 +21,12 @@ const drawImage =() =>{
     j = canvas.width
   }
 
-  
-
   context.drawImage(image, i, 0, canvas.width, canvas.height);
   context.drawImage(image, j, 0, canvas.width, canvas.height);
 
-  let animation = requestAnimationFrame(draw);
+let animation = requestAnimationFrame
   
-}
-draw();
+  
+
+  }
+draw()
